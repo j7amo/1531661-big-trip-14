1fd028d93db092ef966d7659db4d5f24ddd8f872
j7amo <roman.tarnakin@gmail.com>
2021-04-28 15:18:59 +0300

    Добавит перечисления действий пользователя и типов обновления. Добавит методы обновления, добавления и удаления точки маршрута в классе модели точек маршрута

--- src/const.js
+++ src/const.js
@@ -4,4 +4,18 @@ const SortType = {
   SORT_BY_PRICE_DOWN: 'sort-by-price-down',
 };
 
-export { SortType };
+// добавим перечисление для действий пользователя
+const UserAction = {
+  UPDATE_TASK: 'UPDATE_TASK',
+  ADD_TASK: 'ADD_TASK',
+  DELETE_TASK: 'DELETE_TASK',
+};
+
+// добавим перечисление для типа обновления данных
+const UpdateType = {
+  PATCH: 'PATCH',
+  MINOR: 'MINOR',
+  MAJOR: 'MAJOR',
+};
+
+export { SortType, UserAction, UpdateType };
